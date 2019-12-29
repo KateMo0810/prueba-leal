488085d0781d9a17b19eb497befafdbe8ce6a676
Kate Morales <katemorales@iMac-de-Kate.local>
2019-12-28 20:51:32 -0500

    exportTransactionsToExcel

--- src/routes/users.js
+++ src/routes/users.js
@@ -4,6 +4,7 @@ const pool = require('../../database')
 const { verifyToken } = require('../lib/tokens')
 const md5 = require('md5')
 const ExcelJS = require('exceljs');
+const path = require('path')
 
 router.get('/getTransactionHistory', verifyToken, async(req, res) => {
     if (req.token) {
@@ -22,22 +23,38 @@ router.get('/getPoints', verifyToken, async(req, res) => {
     }
 })
 
-router.get('/exportTransactionsToExcel', (req, res) => {
-    var Excel = require('exceljs');
-    var workbook = new Excel.Workbook();
-    var worksheet = workbook.addWorksheet('My Sheet');
-    worksheet.columns = [
-        { header: 'Id', key: 'id', width: 10 },
-        { header: 'Name', key: 'name', width: 32 },
-        { header: 'D.O.B.', key: 'DOB', width: 10 }
-    ];
-    worksheet.addRow({ id: 1, name: 'John Doe', dob: new Date(1970, 1, 1) });
-    worksheet.addRow({ id: 2, name: 'Jane Doe', dob: new Date(1965, 1, 7) });
+router.get('/exportTransactionsToExcel', verifyToken, async(req, res) => {
+    if (req.token) {
+        const id = md5(req.token.email)
+        const { email } = req.token
+        const links = await pool.query('SELECT * FROM _transaction WHERE user_id= ? ORDER BY created_date DESC', [id])
 
-    workbook.xlsx.writeFile('./temp.xlsx').then(function() {
-        // done
-        console.log('file is written');
-    });
+        var Excel = require('exceljs');
+        var workbook = new Excel.Workbook();
+        var worksheet = workbook.addWorksheet('Transacciones');
+        worksheet.columns = [
+            { header: 'Transaction_Id', key: 'transaction_id', width: 20, style: { font: { name: 'Calibri', size: 14 } } },
+            { header: 'Created_Date', key: 'created_date', width: 22, style: { numFmt: 'dd/mm/yyyy hh-mm-ss', font: { name: 'Calibri', size: 14 } } },
+            { header: 'Value', key: 'value', width: 20, style: { font: { name: 'Calibri', size: 14 } } },
+            { header: 'Points', key: 'points', width: 20, style: { font: { name: 'Calibri', size: 14 } } },
+            { header: 'User_Id', key: 'user_id', width: 39, style: { font: { name: 'Calibri', size: 14 } } }
+        ];
+
+        worksheet.getRow(1).fill = {
+            type: 'pattern',
+            pattern: 'solid',
+            fgColor: { argb: 'cccccc' }
+        }
+        links.forEach(element => {
+            worksheet.addRow({ transaction_id: element.transaction_id, created_date: element.created_date, value: element.value, points: element.points, user_id: element.user_id });
+        });
+        workbook.xlsx.writeFile('../../transactions_' + email + '.xlsx').then(function() {
+            res.status(200).send({
+                ok: true,
+                message: 'Documento creado en: ' + path.join(__dirname, '../../transactions_' + email + '.xlsx')
+            })
+        });
+    }
 })
 
 
